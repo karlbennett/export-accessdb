cb6872b896bcfa6e7a93ba1a75605b3f866da70c
Karl Bennett <karl@karl-youthnet.(none)>
2010-07-08 23:09:09 +0100

    Finished activity log migration minus the looksups.

--- src/main/java/org/youthnet/export/migration/ActivityLogsMigration.java
+++ src/main/java/org/youthnet/export/migration/ActivityLogsMigration.java
@@ -1,7 +1,7 @@
 package org.youthnet.export.migration;
 
-import org.youthnet.export.domain.vb25.*;
-import org.youthnet.export.domain.vb3.ActivityLogs;
+import org.youthnet.export.domain.vb25.TblActivityLog;
+import org.youthnet.export.domain.vb3.*;
 import org.youthnet.export.io.CSVFileReader;
 import org.youthnet.export.util.CSVUtil;
 
@@ -9,7 +9,7 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -23,36 +23,97 @@ public class ActivityLogsMigration implements Migratable {
         CSVFileReader csvFileReader = null;
         BufferedWriter activityLogsWriter = null;
 
+        BufferedWriter opportunityActLogWriter = null;
+        BufferedWriter organisationActivityLogWriter = null;
+        BufferedWriter volunteerActivityLogWriter = null;
+
         try {
             csvFileReader = new CSVFileReader(new FileReader(csvDir + "tblActivityLog.csv"));
 
             activityLogsWriter = new BufferedWriter(new FileWriter(outputDir + "ActivityLogs.csv"));
 
-            List<TblVol> tblVols = CSVUtil.createDomainList(csvDir + "tblVol.csv", TblVol.class);
-            List<TblOpp> tblOpps =
-                    CSVUtil.createDomainList(csvDir + "tblOpp.csv", TblOpp.class);
-            List<TblOrg> tblOrgs = CSVUtil.createDomainList(csvDir + "tblOrg.csv", TblOrg.class);
-            List<TbluTypeOfActivity> tbluTypeOfActivities =
-                    CSVUtil.createDomainList(csvDir + "tbluTypeOfActivity.csv", TbluTypeOfActivity.class);
+            opportunityActLogWriter = new BufferedWriter(new FileWriter(outputDir + "OpportunityActLogs.csv"));
+            organisationActivityLogWriter = new BufferedWriter(new FileWriter(outputDir + "OrganisationActivityLogs.csv"));
+            volunteerActivityLogWriter = new BufferedWriter(new FileWriter(outputDir + "VolunteerActivityLogs.csv"));
+
+            Map<Long, Opportunities> opportunitiesVb2idMap =
+                    CSVUtil.createVb2idMap(outputDir + "Opportunities.csv", Opportunities.class);
+            Map<Long, Organisations> organisationsVb2idMap =
+                    CSVUtil.createVb2idMap(outputDir + "Organisations.csv", Organisations.class);
+            Map<Long, Contacts> contactsVb2idMap =
+                    CSVUtil.createVb2idMap(outputDir + "Contacts.csv", Contacts.class);
+            Map<Long, Volunteers> volunteersVb2idMap =
+                    CSVUtil.createVb2idMap(outputDir + "Volunteers.csv", Volunteers.class);
+
 
             TblActivityLog tblActivityLog = null;
             ActivityLogs activityLogs = null;
+            OpportunityActLogs linkedOpportunityActLog = null;
+            OpportunityActLogs opportunityActLog = null;
+            OrganisationActivityLogs organisationActivityLog = null;
+            VolunteerActivityLogs volunteerActivityLog = null;
             String record = "";
             while ((record = csvFileReader.readRecord()) != null) {
                 tblActivityLog = new TblActivityLog(record);
                 activityLogs = new ActivityLogs();
 
+                linkedOpportunityActLog = null;
+                opportunityActLog = null;
+
                 activityLogs.setId(UUID.randomUUID());
+                activityLogs.setVbase2Id(tblActivityLog.getLid());
+                activityLogs.setActivityTypeId(null); // TODO: Set the activity log activity type lookup
+                activityLogs.setStartDate(tblActivityLog.getStarttime());
                 activityLogs.setEndDate(tblActivityLog.getEndtime());
                 activityLogs.setIsAllDayEvent(tblActivityLog.getAlldayevent());
                 activityLogs.setNotes(tblActivityLog.getNotes());
-
                 activityLogs.setShowInCalender(tblActivityLog.getShowincalendar());
-                activityLogs.setStartDate(tblActivityLog.getStarttime());
                 activityLogs.setSubject(tblActivityLog.getSubject());
-                activityLogs.setVbase2Id(tblActivityLog.getLid());
+
+                if (tblActivityLog.getLinkedoid() != null) {
+                    linkedOpportunityActLog = new OpportunityActLogs();
+                    linkedOpportunityActLog.setId(UUID.randomUUID());
+                    linkedOpportunityActLog.setOpportunityId(opportunitiesVb2idMap.get(tblActivityLog.getLinkedoid()) == null ?
+                            null : opportunitiesVb2idMap.get(tblActivityLog.getLinkedoid()).getId());
+                }
+
+                if (tblActivityLog.getOid() != null) {
+                    opportunityActLog = new OpportunityActLogs();
+                    opportunityActLog.setId(UUID.randomUUID());
+                    opportunityActLog.setOpportunityId(opportunitiesVb2idMap.get(tblActivityLog.getOid()) == null ?
+                            null : opportunitiesVb2idMap.get(tblActivityLog.getOid()).getId());
+                }
+
+                if (tblActivityLog.getOrgid() != null) {
+                    organisationActivityLog = new OrganisationActivityLogs();
+                    organisationActivityLog.setId(UUID.randomUUID());
+                    organisationActivityLog.setOrganisationId(opportunitiesVb2idMap.get(tblActivityLog.getOrgid()) == null ?
+                            null : opportunitiesVb2idMap.get(tblActivityLog.getOrgid()).getId());
+
+                    activityLogs.setPersonHereId(contactsVb2idMap.get(tblActivityLog.getOrgid()) == null ?
+                            null : contactsVb2idMap.get(tblActivityLog.getOrgid()).getId());
+                }
+
+                if (tblActivityLog.getVid() != null) {
+                    volunteerActivityLog = new VolunteerActivityLogs();
+                    volunteerActivityLog.setId(UUID.randomUUID());
+                    volunteerActivityLog.setVolunteerId(volunteersVb2idMap.get(tblActivityLog.getVid()) == null ?
+                            null : volunteersVb2idMap.get(tblActivityLog.getVid()).getId());
+                }
+
 
                 activityLogsWriter.write(activityLogs.getRecord() + "\n");
+
+                if (linkedOpportunityActLog != null) {
+                    opportunityActLogWriter.write(linkedOpportunityActLog.getRecord() + "\n");
+                }
+                if (opportunityActLog != null) opportunityActLogWriter.write(opportunityActLog.getRecord() + "\n");
+                if (organisationActivityLog != null) {
+                    organisationActivityLogWriter.write(organisationActivityLog.getRecord() + "\n");
+                }
+                if (volunteerActivityLog != null) {
+                    volunteerActivityLogWriter.write(volunteerActivityLog.getRecord() + "\n");
+                }
             }
 
         } catch (IOException e) {
@@ -60,7 +121,23 @@ public class ActivityLogsMigration implements Migratable {
         } finally {
             try {
                 if (csvFileReader != null) csvFileReader.close();
-                if (activityLogsWriter != null) activityLogsWriter.close();
+                if (activityLogsWriter != null) {
+                    activityLogsWriter.flush();
+                    activityLogsWriter.close();
+                }
+                if (csvFileReader != null) {
+                    opportunityActLogWriter.flush();
+                    opportunityActLogWriter.close();
+                }
+                if (csvFileReader != null) {
+                    organisationActivityLogWriter.flush();
+                    organisationActivityLogWriter.close();
+                }
+                if (csvFileReader != null) {
+                    volunteerActivityLogWriter.flush();
+                    volunteerActivityLogWriter.close();
+                }
+
             } catch (IOException e) {
                 System.out.println("Error closing activity logs streams. Error:" + e.getMessage());
             }
